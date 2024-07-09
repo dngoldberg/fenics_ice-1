47675f7537a9035eac7814f8df3664200aa8b2e8
dngoldberg <31406058+dngoldberg@users.noreply.github.com>
2024-07-09 14:51:34 +0100

    adding back in commands to run full expt

--- example_cases/ismipc_30x30/ismipc_30x30.sh
+++ example_cases/ismipc_30x30/ismipc_30x30.sh
@@ -2,14 +2,14 @@
 set -e
 
 #Generate the input data (100x100 grid, and use this to generate 'obs_vel')
-#python $FENICS_ICE_BASE_DIR/aux/gen_rect_mesh.py -o ./input/momsolve_mesh.xml -xmax 40000 -ymax 40000 -nx 100 -ny 100
-#python $FENICS_ICE_BASE_DIR/aux/gen_rect_mesh.py -o ./input/ismip_mesh.xml -xmax 40000 -ymax 40000 -nx 30 -ny 30
+python $FENICS_ICE_BASE_DIR/aux/gen_rect_mesh.py -o ./input/momsolve_mesh.xml -xmax 40000 -ymax 40000 -nx 100 -ny 100
+python $FENICS_ICE_BASE_DIR/aux/gen_rect_mesh.py -o ./input/ismip_mesh.xml -xmax 40000 -ymax 40000 -nx 30 -ny 30
 
-#python $FENICS_ICE_BASE_DIR/aux/gen_ismipC_domain.py -o ./input/ismipc_input.h5 -L 40000 -nx 100 -ny 100 --reflect
-#python $FENICS_ICE_BASE_DIR/runs/run_momsolve.py momsolve.toml
-#python $FENICS_ICE_BASE_DIR/aux/Uobs_from_momsolve.py -i "U.h5" -o "ismipc_U_obs.h5" -L 40000 -d ./output_momsolve
+python $FENICS_ICE_BASE_DIR/aux/gen_ismipC_domain.py -o ./input/ismipc_input.h5 -L 40000 -nx 100 -ny 100 --reflect
+python $FENICS_ICE_BASE_DIR/runs/run_momsolve.py momsolve.toml
+python $FENICS_ICE_BASE_DIR/aux/Uobs_from_momsolve.py -i "U.h5" -o "ismipc_U_obs.h5" -L 40000 -d ./output_momsolve
 
-#cp output_momsolve/ismipc_U_obs.h5 input/
+cp output_momsolve/ismipc_U_obs.h5 input/
 
 #Run each phase of the model in turn
 RUN_DIR=$FENICS_ICE_BASE_DIR/runs/
